ebc49477b24e439a8a2247cf03d65cd2978015cc
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2023-12-24 09:41:27 +0530

    #

---
+++ app/creators/abhyudaya/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const Error = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4 text-center">
+      
+      <h2 className="text-xl font-medium">{"Hi there, I'm Abhyudaya, 18, and an Indian."}</h2>
+      <h2 className="text-base font-light">Contributors - SA-B <br /><br /> <br /> I think I do have some true friends~ PP-A, SA-B</h2>
+    </div>
+  );
+};
+
+export default Error;

ae95983bdf7546a033cb51d8dc8ee4bbd1de2be5
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2023-12-25 03:27:39 +0530

    Update page.tsx

--- app/creators/abhyudaya/page.tsx
+++ app/creators/abhyudaya/page.tsx
@@ -9,8 +9,12 @@ const Error = () => {
     <div className="h-full flex flex-col items-center justify-center px-6 space-y-4 text-center">
       
       <h2 className="text-xl font-medium">{"Hi there, I'm Abhyudaya, 18, and an Indian."} </h2>
+          <Link href="/creators/sab">
+      <Button size="sm">
           <h2 className="text-base font-medium">Contributor~ SA-B <br/></h2>
-
+        </Button>
+      </Link>
+      <br/>
       <Link href="/">
       <Button size="sm">
           Return Back to ADV Note

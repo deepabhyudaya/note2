d986a6567ba99ffe054bfe9febb87b4899c90738
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2023-12-24 09:27:50 +0530

    *

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+
+export const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
+      
+      <div className="md:ml-auto w-full justify-center md:justify-end flex items-center gap-x-2 text-muted-foreground">
+       
+        <Button variant="ghost" size="sm">
+          ©Abhyudaya 2023
+        </Button>
+      </div>
+    </div>
+  );
+};

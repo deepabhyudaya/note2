b57813bff14cc4a4200b994e379a1958d984782e
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2023-12-24 10:13:31 +0530

    Update page.tsx

--- app/creators/abhyudaya/page.tsx
+++ app/creators/abhyudaya/page.tsx
@@ -8,13 +8,13 @@ const Error = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center px-6 space-y-4 text-center">
       
-      <h2 className="text-xl font-medium">{"Hi there, I'm Abhyudaya, 18, and an Indian."}</h2>
+      <h2 className="text-xl font-medium">{"Hi there, I'm Abhyudaya, 18, and an Indian."} <br /> </h2>
      <Link href="/">
       <Button size="sm">
           Return Back to the WebNote App
         </Button>
       </Link>
-      <h2 className="text-base font-light"><br /> <br /> I guess I do have some true friends~ PP-A, SA-B</h2>
+      <h2 className="text-base font-light"><br /> I guess I do have some true friends~ PP-A, SA-B</h2>
     </div>
   );
 };

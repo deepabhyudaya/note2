7fa447b2932a53277467da1747f2279a018d1c13
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-13 13:41:20 +0530

    Update auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://pumped-pheasant-8.clerk.accounts.dev",
+      domain: "https://prepared-flounder-20.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],

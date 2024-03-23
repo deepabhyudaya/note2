ec8ce685da0d94a8bcd5ede2b2dc7b964bbcdd5d
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-23 20:31:55 +0530

    Update auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,7 +2,7 @@ export default {
   providers: [
     {
       domain: "https://prepared-flounder-20.clerk.accounts.dev",
-      applicationID: "convex",
+      applicationID: "convex", 
     },
   ],
 };
